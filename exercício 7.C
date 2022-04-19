612f62a2d707e136ce1ca26ba7b1ebd25265b02e
Eduardo Lemos <103197804+EduLemos0@users.noreply.github.com>
2022-04-19 16:40:46 -0300

    Update exercício 7.C

--- exercício 7.C
+++ exercício 7.C
@@ -1,25 +1,27 @@
 #include <stdio.h>
 
 //filtra se o número é positivo(true) ou negativo(false)
-void filter(int n){
-  if(n >= 0){
-    printf("True.\n\n");
-  }else{
-    printf("False.\n\n");
+int posNeg(int input){
+  if(input >= 0){
+      return 1;
+    }else{
+      return 0;
   }
 }
 
 
-
-
 // digita quantos números quiser e os coloca em ordem
 int main(void) {
-  int n,i=1;
-  do{
-    printf("%d° número: \n",i);
-    scanf("%d",&n);
-    filter(n);
-    
-   i++;   
-  }while(n != 777);
+  int input,i=1;
+  while(1 ==1) {
+    printf("\nDigite o numero %d: ",i);
+    scanf("%d",&input);
+    posNeg(input);
+    if(posNeg(input) == 1){
+      printf("Positivo");
+    } else {
+      printf("Negativo!");
+    }
+    i++;   
+  }
 }
